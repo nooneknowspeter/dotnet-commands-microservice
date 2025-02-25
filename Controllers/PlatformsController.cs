be44743d3b90f9baab4a0874decfbb49c80edf22
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-25 02:38:49 -0400

    feat: implemented and mapped PlatformReadDto with GetPlatforms endpoint and route

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -8,10 +11,23 @@ namespace CommandsService.Controllers
   [ApiController]
   public class PlatformsController : ControllerBase
   {
+    private readonly ICommandRepo _repository;
+    private readonly IMapper _mapper;
 
-    public PlatformsController()
+    public PlatformsController(ICommandRepo repository, IMapper mapper)
     {
+      _repository = repository;
+      _mapper = mapper;
+    }
 
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+      Console.WriteLine("Getting Platform from CommandsService");
+
+      var platformItems = _repository.GetAllPlatforms();
+
+      return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
     }
 
     [HttpPost]
@@ -21,6 +37,5 @@ namespace CommandsService.Controllers
 
       return Ok("Inbound Succesful");
     }
-
   }
 }
