21250e264b204e10a0288ad6478cf49b67b70292
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-25 02:33:57 -0400

    feat: created interface for Command repository

---
+++ Data/ICommandRepo.cs
@@ -0,0 +1,20 @@
+using CommandsService.Models;
+using System.Collections.Generic;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool saveChanges();
+
+        // platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform plat);
+        bool PlatformExits(int platformId);
+
+        // commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+    }
+}
