a78c586cc7460eff9dc17d237e6ed85e97f8528e
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-03-01 03:22:05 -0400

    feat: added method in interface to get ExternalPlatformID from Platform

--- Data/ICommandRepo.cs
+++ Data/ICommandRepo.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 
 namespace CommandsService.Data
 {
-    public interface ICommandRepo
-    {
-        bool saveChanges();
+  public interface ICommandRepo
+  {
+    bool saveChanges();
 
-        // platforms
-        IEnumerable<Platform> GetAllPlatforms();
-        void CreatePlatform(Platform plat);
-        bool PlatformExits(int platformId);
+    // platforms
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform plat);
+    bool PlatformExits(int platformId);
+    bool ExternalPlatformExists(int externalPlatformId);
 
-        // commands
-        IEnumerable<Command> GetCommandsForPlatform(int platformId);
-        Command GetCommand(int platformId, int commandId);
-        void CreateCommand(int platformId, Command command);
-    }
+    // commands
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+  }
 }
