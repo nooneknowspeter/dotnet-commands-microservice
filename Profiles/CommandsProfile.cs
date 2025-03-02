3ae99889b899326671e7a0ab822a7d848d56bb33
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-03-02 00:22:38 -0400

    feat: mapped incoming gRPC objects to Platform objects

--- Profiles/CommandsProfile.cs
+++ Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CommandsService.Dtos;
 using CommandsService.Models;
+using PlatformService;
 
 namespace CommandsService.Profiles
 {
@@ -15,6 +16,11 @@ namespace CommandsService.Profiles
       // maps Id property from PlatformPublished object to ExternalId to Platform object
       CreateMap<PlatformPublishedDto, Platform>()
         .ForMember(destination => destination.ExternalID, option => option.MapFrom(source => source.Id));
+      // maps incoming gRPC object from server to Platform object
+      CreateMap<GrpcPlatformModel, Platform>()
+        .ForMember(destination => destination.ExternalID, option => option.MapFrom(source => source.PlatformId))
+        .ForMember(destination => destination.Name, option => option.MapFrom(source => source.Name))
+        .ForMember(destination => destination.Commands, option => option.Ignore());
     }
   }
 }
